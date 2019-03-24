8d9f077baf84ca7a79236f8debc10e7220b6c7f5
Maynard Vargas <vargasmaynard@gmail.com>
2019-03-24 19:01:47 +0800

    Start

---
+++ main.js
@@ -0,0 +1,541 @@
+quiz = [
+    {
+       "question":"‘Christ the Redeemer’ is located in which place?",
+       "choices":[
+          "Rio De Janeiro",
+          "Salvador",
+          "Brasilia",
+          "Porto Alegre"
+       ],
+       "answer":"Rio De Janeiro"
+    },
+    {
+       "question":"Which among the following, religion has the highest population in the world?",
+       "choices":[
+          "Hinduism",
+          "Buddhism",
+          "Christianity",
+          "Islam"
+       ],
+       "answer":"Christianity"
+    },
+    {
+       "question":"A premier agency of Intelligence of Pakistan is _",
+       "choices":[
+          "Federal Security Bureau (FSB)",
+          "Inter-Services Intelligence (ISI)",
+          "The Central Intelligence Agency (CIA)",
+          "Ministry of Intelligence"
+       ],
+       "answer":"Inter-Services Intelligence (ISI)"
+    },
+    {
+       "question":"Name the other house of the Parliament of US other than the House of Representatives?",
+       "choices":[
+          "Selicate",
+          "Democratic party",
+          "Setate",
+          "Senate"
+       ],
+       "answer":"Senate"
+    },
+    {
+       "question":"More than 50% of the world’s population is using internet as a part of their daily routine. True or False?",
+       "choices":[
+          "True",
+          "False",
+          "Neither True or False",
+          "Nothing"
+       ],
+       "answer":"False"
+    },
+    {
+       "question":"Select the continent from the below options, which has the lowest internet access?",
+       "choices":[
+          "Europe",
+          "Australia",
+          "America",
+          "Africa"
+       ],
+       "answer":"Africa"
+    },
+    {
+       "question":"The percentage of children died due to the cause of malnutrition is?",
+       "choices":[
+          "40%",
+          "45%",
+          "55%",
+          "65%"
+       ],
+       "answer":"45%"
+    },
+    {
+       "question":"What percentage of world’s terrestrial biodiversity does forest contain?",
+       "choices":[
+          "80%",
+          "90%",
+          "85%",
+          "99%"
+       ],
+       "answer":"80%"
+    },
+    {
+       "question":"Of a ll the wood produced in the world, what percentage of it is used for energy?",
+       "choices":[
+          "55%",
+          "50%",
+          "45%",
+          "60%"
+       ],
+       "answer":"50%"
+    },
+    {
+       "question":"What is the number more than one in which children worldwide under the age of 5 are stunted due to chronic malnutrition?",
+       "choices":[
+          "5",
+          "3",
+          "4",
+          "6"
+       ],
+       "answer":"4"
+    },
+    {
+       "question":"‘Table Mountain’ is located at which place?",
+       "choices":[
+          "Johannesburg , South Africa",
+          "Pretoria, South Africa",
+          "Cape Town, South Africa",
+          "Port Elizabeth, South Africa"
+       ],
+       "answer":"Cape Town, South Africa"
+    },
+    {
+       "question":"Name the city, where Taj Mahal is located in India?",
+       "choices":[
+          "Delhi",
+          "Jaipur",
+          "Agra",
+          "Varanasi"
+       ],
+       "answer":"Agra"
+    },
+    {
+       "question":"Which among the following is a symbol for Peace?",
+       "choices":[
+          "Green eyed monster",
+          "Jade",
+          "Olive Branch",
+          "Heart"
+       ],
+       "answer":"Olive Branch"
+    },
+    {
+       "question":"‘The Colosseum ‘is located in which country?",
+       "choices":[
+          "Venice",
+          "Florence",
+          "Rome",
+          "Milan"
+       ],
+       "answer":"Rome"
+    },
+    {
+       "question":"Which among the following is the world’s largest lake?",
+       "choices":[
+          "Lake Baikal",
+          "Caspian Sea",
+          "Wular Lake",
+          "Lake Erie"
+       ],
+       "answer":"Caspian Sea"
+    },
+    {
+       "question":"Among, the following religion, name the second – highest in the world?",
+       "choices":[
+          "Judaism",
+          "Hinduism",
+          "Islam",
+          "Monothesim"
+       ],
+       "answer":"Islam"
+    },
+    {
+       "question":"Less than what percentage of the 80000 tree species in the world have been studied for their use in any depth?",
+       "choices":[
+          "2%",
+          "3%",
+          "1%",
+          "4%"
+       ],
+       "answer":"1%"
+    },
+    {
+       "question":"What percentage of land area do the mountains cover?",
+       "choices":[
+          "27%",
+          "30%",
+          "24%",
+          "25%"
+       ],
+       "answer":"25%"
+    },
+    {
+       "question":"Nearly one in what number of people around the world do not have access to modern electricity services?",
+       "choices":[
+          "3",
+          "6",
+          "4",
+          "5"
+       ],
+       "answer":"5"
+    },
+    {
+       "question":"The percentage of agriculture land in the total world area is?",
+       "choices":[
+          "41.6%",
+          "39%",
+          "38.5%",
+          "38%"
+       ],
+       "answer":"38.5%"
+    },
+    {
+       "question":"In the 20th century, how much did the water use increased at the rate of population increase?",
+       "choices":[
+          "Once",
+          "Twice",
+          "Tens",
+          "Thrice"
+       ],
+       "answer":"Twice"
+    },
+    {
+       "question":"Who is known as the ‘Father of Biology’?",
+       "choices":[
+          "Galen",
+          "Aristotle",
+          "Socrates",
+          "Avicena"
+       ],
+       "answer":"Aristotle"
+    },
+    {
+       "question":"What percentage of global land area do the forest cover?",
+       "choices":[
+          "31%",
+          "30%",
+          "45%",
+          "35%"
+       ],
+       "answer":"31%"
+    },
+    {
+       "question":"What percentage of the world’s population suffers from hunger?",
+       "choices":[
+          "12%",
+          "15%",
+          "13%",
+          "11%"
+       ],
+       "answer":"11%"
+    },
+    {
+       "question":"The capacity to produce enough food to feed everyone in the world is more adequate .True or False?",
+       "choices":[
+          "True",
+          "False",
+          "Nothing",
+          "Neither true or False"
+       ],
+       "answer":"True"
+    },
+    {
+       "question":"The approximate global population is _",
+       "choices":[
+          "7.6 Million",
+          "7.6 billion",
+          "8 .5 Million",
+          "8.6 Million"
+       ],
+       "answer":"7.6 billion"
+    },
+    {
+       "question":"Which among the following is the largest forest in the world?",
+       "choices":[
+          "Sagano Bamboo Forest",
+          "Taiga",
+          "Crooked Forest",
+          "Black Forest"
+       ],
+       "answer":"Taiga"
+    },
+    {
+       "question":"Which industry is known for being the largest consumers of water?",
+       "choices":[
+          "Aerospace",
+          "Education",
+          "Agriculture",
+          "Chemical"
+       ],
+       "answer":"Agriculture"
+    },
+    {
+       "question":"Which among the following city is called as ‘The Venice of the North ‘?",
+       "choices":[
+          "Oslo",
+          "Stockholm",
+          "Copenhagen",
+          "Helsinki"
+       ],
+       "answer":"Stockholm"
+    },
+    {
+       "question":"‘Brown Revolution’ is associated with _",
+       "choices":[
+          "Avocado",
+          "Mangosteen",
+          "Coconut",
+          "Cocoa"
+       ],
+       "answer":"Cocoa"
+    },
+    {
+       "question":"The symbol for truce is _",
+       "choices":[
+          "Red flag",
+          "Black flag",
+          "White Flag",
+          "Black and white flag"
+       ],
+       "answer":"White Flag"
+    },
+    {
+       "question":"Presently, who is the Chief Executive Officer of Google?",
+       "choices":[
+          "Larry Page",
+          "Steve Ballmer",
+          "Sundar Pichai",
+          "Satya Nadella"
+       ],
+       "answer":"Sundar Pichai"
+    },
+    {
+       "question":"A three-day music festival known as the Orange Festival is celebrated in which country?",
+       "choices":[
+          "Poland",
+          "Finland",
+          "Sweden",
+          "Norway"
+       ],
+       "answer":"Poland"
+    },
+    {
+       "question":"Currently, the CEO of Facebook is _",
+       "choices":[
+          "Priscilla Chan",
+          "Steve Ballmer",
+          "Eduardo Saverin",
+          "Mark Zuckerberg"
+       ],
+       "answer":"Mark Zuckerberg"
+    },
+    {
+       "question":"The Cathedral of Vasily the Blessed, commonly known as the Saint Basils Cathedral is located in which place ?",
+       "choices":[
+          "London",
+          "Kiev",
+          "Moscow",
+          "Saint Petersburg"
+       ],
+       "answer":"Moscow"
+    },
+    {
+       "question":"When did the first modern Olympic Game held?",
+       "choices":[
+          "1897",
+          "1896",
+          "1789",
+          "1869"
+       ],
+       "answer":"1896"
+    },
+    {
+       "question":"The second highest peak of an island in Asia, the Mount Fuji is of which country?",
+       "choices":[
+          "Russia",
+          "Japan",
+          "China",
+          "India"
+       ],
+       "answer":"Japan"
+    },
+    {
+       "question":"What is the currency of Ireland?",
+       "choices":[
+          "Euro",
+          "Dollar",
+          "Pound",
+          "Canadian Dollar"
+       ],
+       "answer":"Euro"
+    },
+    {
+       "question":"What percentage of their income do the poor worldwide spend on food?",
+       "choices":[
+          "30%",
+          "65%",
+          "70%",
+          "75%"
+       ],
+       "answer":"70%"
+    },
+    {
+       "question":"What is the currency of New Zealand?",
+       "choices":[
+          "Croatian kuna",
+          "Swiss franc",
+          "Renminbi",
+          "New Zealand Dollar"
+       ],
+       "answer":"New Zealand Dollar"
+    },
+    {
+       "question":"Which is the tallest waterfall in the world?",
+       "choices":[
+          "Niagara Falls",
+          "Angel Falls",
+          "Iguazu Falls",
+          "Jog Falls"
+       ],
+       "answer":"Angel Falls"
+    },
+    {
+       "question":"Name the person who is known as ‘Father of Medicine ‘?",
+       "choices":[
+          "Galen",
+          "Plato",
+          "Hippocrates",
+          "Herodotus"
+       ],
+       "answer":"Hippocrates"
+    },
+    {
+       "question":"Which is the second tallest waterfall in the world?",
+       "choices":[
+          "Angel Falls",
+          "Tugela Falls",
+          "Tugela Falls",
+          "Gullfoss"
+       ],
+       "answer":"Tugela Falls"
+    },
+    {
+       "question":"The biggest protected area in the world is _",
+       "choices":[
+          "High Line",
+          "Garden of Gods",
+          "Kings Park",
+          "Marae Moana"
+       ],
+       "answer":"Marae Moana"
+    },
+    {
+       "question":"‘International Year of Plant Earth’ was celebrated in _",
+       "choices":[
+          "2008",
+          "2009",
+          "2007",
+          "2010"
+       ],
+       "answer":"2008"
+    },
+    {
+       "question":"Where did the 2018 Winter Olympics held?",
+       "choices":[
+          "North Korea",
+          "Brazil",
+          "USA",
+          "South Korea"
+       ],
+       "answer":"South Korea"
+    },
+    {
+       "question":"The Government of India introduced a new tax system in 2017, name it?",
+       "choices":[
+          "Tariff",
+          "Income Tax",
+          "Goods and Services Tax (GST)",
+          "Payroll Tax"
+       ],
+       "answer":"Goods and Services Tax (GST)"
+    },
+    {
+       "question":"Where did the 1996Summer Olympics, officially known as the Games of XXVI Olympiad held?",
+       "choices":[
+          "Brazil",
+          "USA",
+          "Australia",
+          "UK"
+       ],
+       "answer":"USA"
+    },
+    {
+       "question":"Which among the following is, The Chief Executive Officer of Microsoft?",
+       "choices":[
+          "Satya Nadella",
+          "Steve Ballmer",
+          "Infra Nooyi",
+          "Sundar Pichai"
+       ],
+       "answer":"Satya Nadella"
+    },
+    {
+       "question":"What percentage of the world’s population would be living in cities, by 2030?",
+       "choices":[
+          "60%",
+          "70%",
+          "65%",
+          "75%"
+       ],
+       "answer":"60%"
+    }
+ ]
+
+var score = 0;
+var answer = ''
+
+function game() {
+    var number = Math.floor(Math.random() * 50)
+
+    var question = quiz[number].question
+    var choices = quiz[number].choices
+    answer = quiz[number].answer
+
+
+    document.getElementById('score').innerHTML = score;
+    document.getElementById('question-box').innerHTML = question
+    document.getElementById('button1').innerHTML = choices[0]
+    document.getElementById('button2').innerHTML = choices[1]
+    document.getElementById('button3').innerHTML = choices[2]
+    document.getElementById('button4').innerHTML = choices[3]
+
+}
+
+function clickA(number){
+   console.log("hes");
+   
+    your_answer = document.getElementById('button'+number).innerHTML
+
+    if (your_answer == answer) {
+        score = score + 1
+    } else {
+        score = score - 0.5
+    }
+    game();
+}
+
+function test() {
+   console.log("Hello");
+}
+
+game();
